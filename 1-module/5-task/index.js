8ba159041a9c888f82309323ef3af6d389b053b8
Alexander Khoroshilov <khoroshilov.alex@gmail.com>
2021-04-26 14:44:30 -0700

    Truncate refactoring

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,10 +1,5 @@
 function truncate(str, maxlength) {
-  const TAIL = '…';
-
   if (str.length > maxlength) {
-    str = str.slice(0, maxlength - 1) + TAIL;
-    return str;
+    return `${str.slice(0, maxlength - 1)}…`;
   }
-
-  return str;
 }
