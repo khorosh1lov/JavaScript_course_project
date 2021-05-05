cd338b9e00ab972424d6f1c3503dd9e5f8fcf5c5
Alexander Khoroshilov <khoroshilov.alex@gmail.com>
2021-05-04 18:13:37 -0700

    makeFriendsList()

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,12 @@
-function makeFriendsList(friends) {
-  // ваш код...
+function makeFriendsList(arr) {
+  const friendsList = document.createElement('ul');
+  const data = arr.map(({ firstName, lastName }) => `<li>${firstName} ${lastName}</li>`);
+
+  for (let i = 0; i < data.length; i++) {
+    // friendsList.innerHTML += data[i];
+    // OR
+    friendsList.insertAdjacentHTML('beforeEnd', data[i]);
+  }
+
+  return friendsList;
 }
