39135562c5934f92a9b762742db2c8b0ac8cc200
Alexander Khoroshilov <khoroshilov.alex@gmail.com>
2021-05-05 16:07:49 -0700

    highlight()

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,23 @@
 function highlight(table) {
-  // ваш код...
+  // Get all tbody rows to apply styles/changes
+  const rows = table.querySelectorAll('tbody tr');
+  // Get all last cells with Status
+  const status = table.querySelectorAll('tbody tr > td:last-child');
+
+  for (let i = 0; i < rows.length; i++) {
+    const currentUser = rows[i];
+    const cells = [...rows[i].cells];
+
+    for (let j = 0; j < cells.length; j++) {
+      const data = cells[j];
+
+      if (+data.textContent < 18) { currentUser.style.textDecoration = 'line-through'; }
+      if (data.textContent === 'm') { currentUser.classList.add('male'); }
+      if (data.textContent === 'f') { currentUser.classList.add('female'); }
+      if (data.dataset.available === 'true') { currentUser.classList.add('available'); }
+      if (data.dataset.available === 'false') { currentUser.classList.add('unavailable'); }
+    }
+
+    if (!status[i].hasAttribute('data-available')) { currentUser.setAttribute('hidden', ''); }
+  }
 }
