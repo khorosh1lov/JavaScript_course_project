e45e57e09e10f908b50e21ab18a711f5f4d790c8
Alexander Khoroshilov <khoroshilov.alex@gmail.com>
2021-05-09 21:13:20 -0700

    Carousel() implementation

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,44 @@
+const carousel      = document.querySelector('.carousel__inner');
+const arrowLeft     = document.querySelector('.carousel__arrow_left');
+const arrowRight    = document.querySelector('.carousel__arrow_right');
+const totalSlides   = document.querySelectorAll('.carousel__slide').length;
+let   slidePosition = 0;
+
 function initCarousel() {
-  // ваш код...
+  if (totalSlides <= 1) { disableArrows(arrowLeft, arrowRight); }
+
+  checkSlidePosition();
+  arrowLeft.addEventListener('click', () => { prevSlide(); });
+  arrowRight.addEventListener('click', () => { nextSlide(); });
 }
+
+const prevSlide = () => {
+  slidePosition--;
+  checkSlidePosition();
+  shiftSlide();
+};
+
+const nextSlide = () => {
+  slidePosition++;
+  checkSlidePosition();
+  shiftSlide();
+};
+
+const checkSlidePosition = () => {
+  if (slidePosition === 0) { return disableArrows(arrowLeft); }
+  arrowLeft.style.display = '';
+
+  if (slidePosition === totalSlides - 1) { return disableArrows(arrowRight); }
+  arrowRight.style.display = '';
+};
+
+const shiftSlide = () => {
+  let shiftIn = -carousel.offsetWidth * slidePosition;
+  carousel.style.transform = `translateX(${shiftIn}px)`;
+};
+
+const disableArrows = (...args) => {
+  for (let arg of args) {
+    arg.style.display = 'none';
+  }
+};
