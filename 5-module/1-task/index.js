8bd64e3f61bc2025324b388232507a6aa761ce3d
Alexander Khoroshilov <khoroshilov.alex@gmail.com>
2021-05-11 10:54:34 -0700

    hideSelf() refactoring

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -2,6 +2,6 @@ function hideSelf() {
   const button = document.querySelector('.hide-self-button');
 
   button.addEventListener('click', () => {
-    button.setAttribute('hidden', '');
-  });
+    button.hidden = true;
+  }, { once: true });
 }
