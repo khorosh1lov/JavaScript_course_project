38c4499fabcd4404d3b1fca4e953dc94a2ebdd07
Alexander Khoroshilov <khoroshilov.alex@gmail.com>
2021-05-15 16:22:49 -0700

    class UserTable

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -12,7 +12,60 @@
  *      }
  *
  */
+
+function usersDataTemplate(rows) {
+  return rows.map(({ name, age, salary, city }) => tableRowTemplate({ name, age, salary, city })).join('');
+}
+
+function tableRowTemplate({ name, age, salary, city }) {
+  return `
+  <tr>
+    <td>${name}</td>
+    <td>${age}</td>
+    <td>${salary}</td>
+    <td>${city}</td>
+    <td>${removeButtonTemplate()}</td>
+  </tr>`;
+}
+
+function removeButtonTemplate() {
+  return '<button data-action="remove">X</button>';
+}
+
+function tableContainerTemplate(users) {
+  return `
+    <thead>
+      <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th>Х</th>
+      </tr>
+    </thead>
+    <tbody>
+      ${users}
+    </tbody>`;
+}
+
 export default class UserTable {
   constructor(rows) {
+    this._users = usersDataTemplate(rows);
+
+    this.elem = document.createElement('table');
+    this.elem.setAttribute('data-component', 'tableOfUsers');
+
+    this.elem.addEventListener('click', (e) => {
+      const target = e.target;
+      if (target.dataset.action !== 'remove') {
+        return;
+      }
+
+      const user = target.closest(`tr`);
+      user.remove();
+    });
+
+    this.elem.innerHTML = tableContainerTemplate(this._users);
   }
+
 }
