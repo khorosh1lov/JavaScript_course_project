1199b8d86097e510f806b141378b11658214e219
Alexander Khoroshilov <khoroshilov.alex@gmail.com>
2021-05-21 17:56:53 -0700

    class StepSlider refactoring

--- 7-module/4-task/index.js
+++ 7-module/4-task/index.js
@@ -40,6 +40,7 @@ export default class StepSlider {
   constructor({ steps, value = 0 }) {
     this._totalSteps = steps;
     this._startValue = value;
+
     // Creating our Main Element with Slider
     this._elem       = createElement(sliderTemplate(this._totalSteps, this._startValue));
 
@@ -81,33 +82,31 @@ export default class StepSlider {
     this._thumb.style.zIndex = 1000;
     this._elem.classList.add('slider_dragging');
 
-    const onMove = (e) => {
-      // Counting where exactly user clicked
-      let thumbPositionClicked = (e.clientX - this._elem.getBoundingClientRect().left) / this._elem.offsetWidth;
-      if (thumbPositionClicked < 0) { thumbPositionClicked = 0; }
-      if (thumbPositionClicked > 1) { thumbPositionClicked = 1; }
-
-      const approxValue = Math.round(thumbPositionClicked * this._segments);
-      const approxValueInPercents = thumbPositionClicked * 100;
-      const approxStep = this._elem.querySelector(`[data-step="${approxValue}"]`);
-
-      this._allSteps.forEach(approxStep => { approxStep.classList.remove('slider__step-active'); });
-      approxStep.classList.add('slider__step-active');
-
-      // Moving Thumb and Values
-      this._value.innerHTML = approxValue;
-      this._thumb.style.left = `${approxValueInPercents}%`;
-      this._progress.style.width = `${approxValueInPercents}%`;
-    };
-
-    document.addEventListener('pointermove', onMove);
+    document.addEventListener('pointermove', this._onMove);
 
     document.addEventListener('pointerup', () => {
       this._elem.classList.remove('slider_dragging');
-      document.removeEventListener('pointermove', onMove);
+      document.removeEventListener('pointermove', this._onMove);
     }, { once: true });
+  };
 
+  _onMove = (e) => {
+    // Counting where exactly user clicked
+    let thumbPositionClicked = (e.clientX - this._elem.getBoundingClientRect().left) / this._elem.offsetWidth;
+    if (thumbPositionClicked < 0) { thumbPositionClicked = 0; }
+    if (thumbPositionClicked > 1) { thumbPositionClicked = 1; }
 
+    const approxValue = Math.round(thumbPositionClicked * this._segments);
+    const approxValueInPercents = thumbPositionClicked * 100;
+    const approxStep = this._elem.querySelector(`[data-step="${approxValue}"]`);
+
+    this._allSteps.forEach(approxStep => { approxStep.classList.remove('slider__step-active'); });
+    approxStep.classList.add('slider__step-active');
+
+    // Moving Thumb and Values
+    this._value.innerHTML = approxValue;
+    this._thumb.style.left = `${approxValueInPercents}%`;
+    this._progress.style.width = `${approxValueInPercents}%`;
   };
 
   // stopPropagation & preventDefault for Sensor Devices
