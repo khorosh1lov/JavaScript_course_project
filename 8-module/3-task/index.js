dfc81304700c7433b6b102b6577852a631f61f8e
Alexander Khoroshilov <khoroshilov.alex@gmail.com>
2021-05-26 10:32:12 -0700

    class Cart - part #1

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -1,19 +1,31 @@
 export default class Cart {
-  cartItems = []; // [product: {...}, count: N]
+  cartItems = []; // [ product: {...}, count: N ]
 
   constructor(cartIcon) {
     this.cartIcon = cartIcon;
   }
 
   addProduct(product) {
-    let AddProduct = Object.assign({}, product);
-    this.cartItems.push(AddProduct);
+    const existingProduct = this._findProductByName(product);
+    const cartItem = Object.assign({}, { product: product, count: 1 });
 
-    this.onProductUpdate();
+    if (!existingProduct) {
+      this.cartItems.push(cartItem);
+    } else {
+      existingProduct.count++;
+    }
+
+    this.onProductUpdate(cartItem);
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    const existingProduct = this._findProductById(productId);
+    const indexOfExistingProduct = this._findProductIndexById(productId);
+
+    if (existingProduct) { existingProduct.count += amount; } else { return; }
+    if (existingProduct.count === 0) { this.cartItems.splice(indexOfExistingProduct, 1); }
+
+    this.onProductUpdate(existingProduct);
   }
 
   isEmpty() {
@@ -21,19 +33,29 @@ export default class Cart {
   }
 
   getTotalCount() {
-    return this.cartItems.length;
+    return this.cartItems.reduce((total, item) => total + item.count, 0);
   }
 
   getTotalPrice() {
-    return this.cartItems.reduce((totalPrice, product) => totalPrice + product.price, 0);
+    return this.cartItems.reduce((total, item) => total + (item.product.price * item.count), 0);
   }
 
   onProductUpdate() {
     // реализуем в следующей задаче
 
-    console.log(this.cartItems);
-
     this.cartIcon.update(this);
   }
+
+  _findProductByName = (product) => {
+    return this.cartItems.find(item => item.product.name === product.name);
+  };
+
+  _findProductById = (productId) => {
+    return this.cartItems.find(item => item.product.id === productId);
+  };
+
+  _findProductIndexById = (productId) => {
+    return this.cartItems.findIndex(item => item.product.id === productId);
+  };
 }
 
